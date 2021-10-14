2458edad145c92cc609821cd70804c9eb3e4eaca
Giovanni Malcolm <giovannimalcolm@giovannis-mbp.lan>
2021-10-14 18:41:00 -0400

    misc. smaller functions added. going to work on api call functions now

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,43 +15,86 @@ var searchBox = document.querySelector("#citySearch");
 
 
 //Function to pull history from user lcl storage
-function getHistory (){
+function getHistory() {
     var storedHistory = localStorage.getItem('searchHistory')
-    if (storedHistory){
+    if (storedHistory) {
         searchHistory = JSON.parse(storedHistory);
     }
-    
-/// add function to print history after parsed
+
+    /// add function to print history after parsed
 }
 
-function addHistory (input){
+function addHistory(input) {
 
-    if (searchHistory.indexOf(input) == -1){
+    if (searchHistory.indexOf(input) == -1) {
         return;
     }
     searchHistory.push(input);
 
-    localStorage.setItem('history',JSON.stringify(searchHistory));
+    localStorage.setItem('history', JSON.stringify(searchHistory));
     printHistory(); //create function
 }
 
-function printHistory () {
+function printHistory() {
     historyContainer.innerHTML = ''; //clear it out before printing so no doubles
     //end to start of list, create button with city names 
-    for (var i =searchHistory.length -1; i >= 0; i--){
+    for (var i = searchHistory.length - 1; i >= 0; i--) {
         var historyItem = document.createElement('button');
         historyItem.textContent = searchHistory[i];
         historyContainer.append(historyItem);
     }
-    
+
 }
 
 
-searchBox.addEventListener('submit', searchCity)
 
-function searchCity(){
 
-    
+
+function fetchLoc(fixedInput) {
+    var requestURL = `${weatherApiRootUrl}/geo/1.0/direct?q=${search}&limit=5&appid=${weatherApiKey}`;
+    fetch(requestURL)
+        .then(function (res) {
+            return res.json();
+        })
+        .then(function (data) {
+            if (data[0] = null) {
+                alert('City not found');
+            }
+            else {
+                addHistory(fixedInput)
+                ///get weather
+            }
+        })
+        .catch(function (err) {
+            console.error(err);
+        });
+}
+
+
+
+function historySearch(e){
+    if (!e.target.matches('.inHistory')){
+        return;
+    }
+
+    var hist = e.target;
+    var histChosen = hist.getAttribute('data-search');
+    fetchLoc(histChosen);
+
+}
+
+function searchCity(e) {
+
+    if (cityInput = null) {
+        return;
+    }
+    e.preventDefault();
+    var fixedInput = cityInput.value.trim();
+    fetchLoc(fixedInput);
+    cityInput.value = '';
 }
 
 getHistory();
+searchBox.addEventListener('submit', searchCity)
+historyContainer.addEventListener('click', historySearch)
+
